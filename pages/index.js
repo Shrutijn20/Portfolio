416c015811637532d4fc34cf0a679c0a46b79278
Shruti Jain <20shrutujn@gmail.com>
2023-07-17 20:02:17 +0530

    second

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import About from '../components/About'
 import Contact from '../components/Contact'
 import Main from '../components/Main'
 import Projects from '../components/Projects'
-import Skills from '../components/skills'
+import Skills from '../components/Skills'
 
 
 export default function Home() {
